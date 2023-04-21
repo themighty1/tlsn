2675b1b40b513644a9fb9e6062c5dd5545a662ee
th4s <th4s@metavoid.xyz>
2023-04-21 15:33:54 +0200

    WIP: Figuring out how this works...

--- tls/prover/src/buffer.rs
+++ tls/prover/src/buffer.rs
@@ -1,28 +1,53 @@
-use futures::io::{AsyncRead, AsyncWrite};
 use std::{
     io::Error,
-    net::TcpStream,
     pin::Pin,
+    sync::{atomic::AtomicUsize, Arc, Mutex},
     task::{Context, Poll},
 };
 
-pub struct RequestBuffer(pub Vec<u8>);
+use futures::{AsyncRead, AsyncWrite};
+
+pub struct ExchangeBuffer {
+    request_buffer: Arc<Mutex<Vec<u8>>>,
+    response_buffer: Arc<Mutex<Vec<u8>>>,
+}
+
+impl ExchangeBuffer {
+    pub fn new(size: usize) -> Self {
+        Self {
+            request_buffer: Arc::new(Mutex::new(vec![0; size])),
+            response_buffer: Arc::new(Mutex::new(vec![0; size])),
+        }
+    }
+
+    pub fn request_buffer(&self) -> RequestBuffer {
+        RequestBuffer(self.request_buffer.clone())
+    }
+
+    pub async fn write_request(&mut self) -> Result<(), Error> {
+        todo!();
+    }
+
+    pub async fn read_request(&mut self) -> Result<(), Error> {
+        todo!();
+    }
+}
 
 impl AsyncWrite for RequestBuffer {
     fn poll_write(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, Error>> {
-        todo!()
+        Pin::new(&mut self.as_mut().0).poll_write(cx, buf)
     }
 
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Pin::new(&mut self.as_mut().0).poll_close(cx)
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Pin::new(&mut self.as_mut().0).poll_flush(cx)
     }
 }
 
@@ -34,6 +59,6 @@ impl AsyncRead for ResponseBuffer {
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<Result<usize, Error>> {
-        todo!()
+        Pin::new(&mut self.0.as_slice()).poll_read(cx, buf)
     }
 }
--- tls/prover/src/lib.rs
+++ tls/prover/src/lib.rs
@@ -1,12 +1,6 @@
 use buffer::{RequestBuffer, ResponseBuffer};
 use config::ProverConfig;
-use futures::{AsyncRead, AsyncWrite};
-use std::{
-    io::{Error, Read},
-    net::TcpStream,
-    pin::Pin,
-    task::{Context, Poll},
-};
+use std::{future::Future, io::Read, net::TcpStream};
 use tls_client::ClientConnection;
 
 mod buffer;
@@ -24,49 +18,24 @@ impl Prover {
         todo!();
     }
 
-    pub async fn run(&mut self) -> Result<(), ProverError> {
-        loop {
-            // Pull requests from the request buffer
-            self.tls_connection.read_tls(&mut self.tcp_stream)?;
-            self.tls_connection
-                .reader()
-                .read_to_end(&mut self.response_buffer.0)?;
-            self.tls_connection.process_new_packets().await?;
+    // Caller needs to run future on executor
+    pub fn run(&mut self) -> impl Future<Output = Result<(), ProverError>> + '_ {
+        async move {
+            loop {
+                // Pull requests from the request buffer
+                self.tls_connection.read_tls(&mut self.tcp_stream)?;
+                self.tls_connection
+                    .reader()
+                    .read_to_end(&mut self.response_buffer.0)?;
+                self.tls_connection.process_new_packets().await?;
 
-            // Push responses into the response buffer
-            self.tls_connection
-                .write_all_plaintext(&self.request_buffer.0)
-                .await?;
-            self.tls_connection.write_tls(&mut self.tcp_stream)?;
+                // Push responses into the response buffer
+                self.tls_connection
+                    .write_all_plaintext(&self.request_buffer.0)
+                    .await?;
+                self.tls_connection.write_tls(&mut self.tcp_stream)?;
+            }
         }
-    }
-}
-
-impl AsyncRead for Prover {
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut [u8],
-    ) -> Poll<Result<usize, Error>> {
-        Pin::new(&mut self.response_buffer).poll_read(cx, buf)
-    }
-}
-
-impl AsyncWrite for Prover {
-    fn poll_write(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<Result<usize, Error>> {
-        Pin::new(&mut self.request_buffer).poll_write(cx, buf)
-    }
-
-    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        Pin::new(&mut self.request_buffer).poll_close(cx)
-    }
-
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        Pin::new(&mut self.request_buffer).poll_flush(cx)
     }
 }
 
