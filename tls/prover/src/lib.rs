f791e1e09c2b7feb2c0e2318a269d3649873fd81
th4s <th4s@metavoid.xyz>
2023-05-05 11:52:52 +0200

    Added channels and removed buffer

--- tls/prover/src/lib.rs
+++ tls/prover/src/lib.rs
@@ -1,51 +1,10 @@
-use config::ProverConfig;
-use std::{
-    future::Future,
-    io::{Read, Write},
-    net::TcpStream,
-};
-use tls_client::ClientConnection;
+use self::prover::ProverError;
 
-mod buffer;
-mod config;
+mod prover;
 
-pub struct Prover {
-    tls_connection: ClientConnection,
-    tcp_stream: TcpStream,
-    //buffer: Buffer,
-}
+pub use prover::Prover;
 
-impl Prover {
-    pub fn new(_config: ProverConfig) -> Self {
-        todo!();
-    }
-
-    // Caller needs to run future on executor
-    pub fn run(&mut self) -> impl Future<Output = Result<(), ProverError>> + '_ {
-        async move {
-            loop {
-                // Pull requests from the request buffer
-                //   self.tls_connection.read_tls(&mut self.tcp_stream)?;
-                //   self.tls_connection.process_new_packets().await?;
-                //   self.tls_connection
-                //       .reader()
-                //       .read_to_end(self.buffer.request_buffer())?;
-
-                // Push responses into the response buffer
-                //self.tls_connection
-                //    .write_all_plaintext(self.buffer.response_buffer())
-                //    .await?;
-                //self.tls_connection.write_tls(&mut self.tcp_stream)?;
-            }
-            Ok(())
-        }
-    }
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum ProverError {
-    #[error("TLS client error: {0}")]
-    TlsClientError(#[from] tls_client::Error),
-    #[error("IO error: {0}")]
-    IOError(#[from] std::io::Error),
+pub trait Prove {
+    fn make_request<T: Into<Vec<u8>>>(&self, request: T) -> Result<(), ProverError>;
+    fn get_response(&self) -> Result<Vec<u8>, ProverError>;
 }
