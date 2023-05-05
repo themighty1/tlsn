a276216db993b2179118b4bf52f778fde95e4ac1
th4s <th4s@metavoid.xyz>
2023-05-05 12:52:56 +0200

    Added draft for config and removed tcp stream
    - also improved code here and there

--- tls/prover/src/lib.rs
+++ tls/prover/src/lib.rs
@@ -2,7 +2,7 @@ use self::prover::ProverError;
 
 mod prover;
 
-pub use prover::Prover;
+pub use crate::prover::Prover;
 
 pub trait Prove {
     fn make_request<T: Into<Vec<u8>>>(&self, request: T) -> Result<(), ProverError>;
--- tls/prover/src/prover/config.rs
+++ tls/prover/src/prover/config.rs
@@ -1,3 +1,29 @@
+use tls_client::{ClientConfig, OwnedTrustAnchor, RootCertStore};
+
 pub struct ProverConfig {
+    pub client_config: ClientConfig,
     // ...
+}
+
+impl Default for ProverConfig {
+    fn default() -> Self {
+        let client_config = ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(add_mozilla_roots())
+            .with_no_client_auth();
+
+        Self { client_config }
+    }
+}
+
+fn add_mozilla_roots() -> RootCertStore {
+    let mut root_store = RootCertStore::empty();
+    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+        OwnedTrustAnchor::from_subject_spki_name_constraints(
+            ta.subject,
+            ta.spki,
+            ta.name_constraints,
+        )
+    }));
+    root_store
 }
