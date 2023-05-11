4aee189ef0dec5956a66d208c870d8c272716471
th4s <th4s@metavoid.xyz>
2023-05-11 20:58:49 +0200

    Separated into 4 futures

--- tls/prover/src/prover/mod.rs
+++ tls/prover/src/prover/mod.rs
@@ -16,17 +16,19 @@ where
 {
     tls_connection: ClientConnection,
     socket: TcpStream,
-    request_sender: Option<Sender<T>>,
-    request_receiver: Receiver<T>,
-    response_sender: Sender<T>,
-    response_receiver: Option<Receiver<T>>,
+    request_receiver: Option<Receiver<T>>,
+    response_sender: Option<Sender<T>>,
 }
 
 impl<T> Prover<T>
 where
     T: From<Vec<u8>> + Into<Vec<u8>> + std::fmt::Debug + Send + 'static,
 {
-    pub fn new(config: ProverConfig, url: String, socket: TcpStream) -> Result<Self, ProverError> {
+    pub fn new(
+        config: ProverConfig,
+        url: String,
+        socket: TcpStream,
+    ) -> Result<(Self, Sender<T>, Receiver<T>), ProverError> {
         let backend = Box::new(tls_client::TLSNBackend {});
         Self::new_with(config, url, backend, socket)
     }
@@ -36,7 +38,7 @@ where
         config: ProverConfig,
         url: String,
         socket: TcpStream,
-    ) -> Result<Self, ProverError> {
+    ) -> Result<(Self, Sender<T>, Receiver<T>), ProverError> {
         let backend = Box::new(tls_client::RustCryptoBackend::new());
         Self::new_with(config, url, backend, socket)
     }
@@ -46,7 +48,7 @@ where
         url: String,
         backend: Box<dyn Backend>,
         socket: TcpStream,
-    ) -> Result<Self, ProverError> {
+    ) -> Result<(Self, Sender<T>, Receiver<T>), ProverError> {
         let (request_sender, request_receiver) = futures::channel::mpsc::channel(10);
         let (response_sender, response_receiver) = futures::channel::mpsc::channel(10);
 
@@ -57,55 +59,56 @@ where
         let prover = Self {
             tls_connection,
             socket,
-            request_sender: Some(request_sender),
-            request_receiver,
-            response_sender,
-            response_receiver: Some(response_receiver),
+            request_receiver: Some(request_receiver),
+            response_sender: Some(response_sender),
         };
-        Ok(prover)
-    }
-
-    pub fn take_channels(&mut self) -> Result<(Sender<T>, Receiver<T>), ProverError> {
-        let request_channel = self
-            .request_sender
-            .take()
-            .ok_or(ProverError::NoChannelsAvailable)?;
-        let response_channel = self
-            .response_receiver
-            .take()
-            .ok_or(ProverError::NoChannelsAvailable)?;
-
-        Ok((request_channel, response_channel))
+        Ok((prover, request_sender, response_receiver))
     }
 
     // Caller needs to run future on executor
-    pub fn run(self, executor: impl Spawn) {
+    pub fn run(mut self, executor: impl Spawn) {
         // TODO Currently two problems:
         // 1. The `request_receiver.next()` call blocks if there is no request
         // 2. For some reason `tls_connection.wants_write()` is always `false
 
         // TODO There should be a better option than using an await mutex
-        let prover_read_mutex = Arc::new(Mutex::new(self));
-        let prover_write_mutex = Arc::clone(&prover_read_mutex);
-        println!("At beginning of  prover run");
+        let mut sender = self.response_sender.take().unwrap();
+        let mut receiver = self.request_receiver.take().unwrap();
+        let prover_mutex = Arc::new(Mutex::new(self));
+        let prover_mutex2 = Arc::clone(&prover_mutex);
+        let prover_mutex3 = Arc::clone(&prover_mutex);
+        let prover_mutex4 = Arc::clone(&prover_mutex);
+        println!("At beginning of prover run");
 
         let write_fut = async move {
             loop {
                 {
                     println!("in write loop");
-                    let mut prover = prover_write_mutex.lock().await;
-                    let mut socket = prover.socket.try_clone()?;
+                    let mut prover = prover_mutex.lock().await;
                     // Push requests into the TCP stream
-                    if let Some(request) = prover.request_receiver.next().await {
+                    if let Some(request) = receiver.next().await {
                         prover
                             .tls_connection
                             .write_all_plaintext(request.into().as_slice())
                             .await?;
                     }
+                }
+                println!("end of write loop");
+            }
+            Ok::<(), ProverError>(())
+        };
+
+        let write_tls_fut = async move {
+            loop {
+                {
+                    println!("in write tls loop");
+                    let mut prover = prover_mutex2.lock().await;
+                    let mut socket = prover.socket.try_clone()?;
                     if prover.tls_connection.wants_write() {
                         prover.tls_connection.write_tls(&mut socket)?;
                     }
                 }
+                println!("end of write tls loop");
             }
             Ok::<(), ProverError>(())
         };
@@ -114,25 +117,37 @@ where
             loop {
                 {
                     println!("in read loop");
-                    let mut prover = prover_read_mutex.lock().await;
-                    let mut socket = prover.socket.try_clone()?;
-                    // Pull responses from the TCP stream
+                    let mut prover = prover_mutex3.lock().await;
                     let mut response = Vec::new();
-                    if prover.tls_connection.wants_read() {
-                        prover.tls_connection.read_tls(&mut socket)?;
-                        prover.tls_connection.process_new_packets().await?;
-                        prover.tls_connection.reader().read_to_end(&mut response)?;
-                    }
-
+                    prover.tls_connection.reader().read_to_end(&mut response)?;
                     if !response.is_empty() {
-                        prover.response_sender.send(response.into()).await?;
+                        sender.send(response.into()).await?;
                     }
                 }
+                println!("end of read loop");
             }
             Ok::<(), ProverError>(())
         };
+
+        let read_tls_fut = async move {
+            loop {
+                {
+                    println!("in read tls loop");
+                    let mut prover = prover_mutex4.lock().await;
+                    let mut socket = prover.socket.try_clone()?;
+                    // Pull responses from the TCP stream
+                    if prover.tls_connection.wants_read() {
+                        prover.tls_connection.read_tls(&mut socket)?;
+                        prover.tls_connection.process_new_packets().await?;
+                    }
+                }
+                println!("end of read tls loop");
+            }
+
+            Ok::<(), ProverError>(())
+        };
         let fut = async {
-            try_join!(write_fut, read_fut).unwrap();
+            try_join!(write_fut, write_tls_fut, read_fut, read_tls_fut).unwrap();
         };
         executor.spawn(fut).unwrap();
     }
@@ -144,8 +159,6 @@ pub enum ProverError {
     TlsClientError(#[from] tls_client::Error),
     #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
-    #[error("Channels for sending requests and receiving responses have already been assigned")]
-    NoChannelsAvailable,
     #[error("Unable to deliver response")]
     ResponseError(#[from] futures::channel::mpsc::SendError),
     #[error("Unable to parse URL: {0}")]
@@ -166,13 +179,13 @@ mod tests {
 
         let tcp_stream = std::net::TcpStream::connect("tlsnotary.org:443").unwrap();
 
-        let mut prover = Prover::<Vec<u8>>::new_with_standard(
-            ProverConfig::default(),
-            String::from("tlsnotary.org"),
-            tcp_stream,
-        )
-        .unwrap();
-        let (mut request_channel, mut response_channel) = prover.take_channels().unwrap();
+        let (prover, mut request_channel, mut response_channel) =
+            Prover::<Vec<u8>>::new_with_standard(
+                ProverConfig::default(),
+                String::from("tlsnotary.org"),
+                tcp_stream,
+            )
+            .unwrap();
         prover.run(rt.compat());
         println!("Started prover run loop");
 
