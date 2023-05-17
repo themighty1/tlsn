3d72f3aa9eebbc73fab1528c479effbd44d5fe53
th4s <th4s@metavoid.xyz>
2023-05-17 19:08:23 +0200

    Refined async socket

--- tls/prover/src/lib.rs
+++ tls/prover/src/lib.rs
@@ -1,15 +1,14 @@
+use bytes::Bytes;
 use futures::{
-    channel::mpsc::{Receiver, Sender},
     task::{Spawn, SpawnExt},
-    AsyncRead, AsyncWrite, Future, SinkExt, StreamExt,
+    Future,
 };
 use std::{
-    io::{ErrorKind, Read},
-    net::TcpStream,
+    io::{Read, Write},
+    pin::Pin,
     sync::Arc,
 };
 use tls_client::{client::InvalidDnsNameError, Backend, ClientConnection, ServerName};
-use tokio::sync::Mutex;
 
 mod config;
 mod socket;
@@ -18,117 +17,73 @@ pub use config::ProverConfig;
 
 use self::socket::AsyncSocket;
 
-pub trait AsyncReadWrite: AsyncRead + AsyncWrite + Unpin + Send + Sync {}
-impl<T> AsyncReadWrite for T where T: AsyncRead + AsyncWrite + Unpin + Send + Sync {}
+pub trait ReadWrite: Read + Write + Unpin + Send {}
+impl<T> ReadWrite for T where T: Read + Write + Unpin + Send {}
 
 pub struct Prover {
-    run_future: FnOnce() -> impl Future<Output = ()>,
+    run_future: Option<Pin<Box<dyn Future<Output = ()> + Send + 'static>>>,
 }
 
 impl Prover {
-    fn new(
+    pub fn new(
         config: ProverConfig,
         url: String,
         backend: Box<dyn Backend>,
-        socket: Box<dyn AsyncReadWrite>,
+        mut socket: Box<dyn ReadWrite>,
     ) -> Result<(Self, AsyncSocket), ProverError> {
-        let (request_sender, request_receiver) = futures::channel::mpsc::channel(10);
-        let (response_sender, response_receiver) = futures::channel::mpsc::channel(10);
+        let (request_sender, mut request_receiver) = tokio::sync::mpsc::channel::<Bytes>(10);
+        let (response_sender, response_receiver) =
+            tokio::sync::mpsc::channel::<Result<Bytes, std::io::Error>>(10);
 
         let async_socket = AsyncSocket::new(request_sender, response_receiver);
 
         let server_name = ServerName::try_from(url.as_str())?;
         let client_config = Arc::new(config.client_config);
-        let tls_connection = ClientConnection::new(client_config, backend, server_name)?;
+        let mut tls_conn = ClientConnection::new(client_config, backend, server_name)?;
 
-        self.run_future = async {};
+        let run_future = async move {
+            // Handshake
+            tls_conn.start().await.unwrap();
+
+            // Accept requests and write them to the tls connection
+            if let Some(request) = request_receiver.recv().await {
+                tls_conn
+                    .write_all_plaintext(request.as_ref())
+                    .await
+                    .unwrap();
+            }
+
+            // Write the encrypted tls traffic into the socket
+            if tls_conn.wants_write() {
+                tls_conn.write_tls(&mut socket).unwrap();
+            }
+
+            // Read the encrypted tls traffic from the socket
+            if tls_conn.wants_read() {
+                tls_conn.read_tls(&mut socket).unwrap();
+                tls_conn.process_new_packets().await.unwrap();
+            }
+
+            // Decrypt the tls traffic and send it to the response channel
+            let mut response = Vec::new();
+            tls_conn.reader().read_to_end(&mut response).unwrap();
+            if !response.is_empty() {
+                response_sender.send(Ok(response.into())).await.unwrap();
+            }
+        };
+
+        let prover = Prover {
+            run_future: Some(Box::pin(run_future)),
+        };
 
         Ok((prover, async_socket))
     }
 
     // Caller needs to run future on executor
-    pub fn run(mut self, executor: impl Spawn) {
-        let response_sender = self
-            .response_sender
-            .take()
-            .expect("Prover is already running");
-        let request_receiver = self
-            .request_receiver
-            .take()
-            .expect("Prover is already running");
-        let prover = Arc::new(self);
-
-        executor.spawn(prover.clone().handshake()).unwrap();
-        executor
-            .spawn(prover.clone().write(request_receiver))
-            .unwrap();
-        executor.spawn(prover.clone().write_tls()).unwrap();
-        executor
-            .spawn(prover.clone().read(response_sender))
-            .unwrap();
-        executor.spawn(prover.clone().read_tls()).unwrap();
-    }
-
-    async fn handshake(self: Arc<Self>) {
-        self.tls_connection.lock().await.start().await.unwrap()
-    }
-
-    async fn write(self: Arc<Self>, mut request_receiver: Receiver<Vec<u8>>) {
-        loop {
-            if let Some(request) = request_receiver.next().await {
-                self.tls_connection
-                    .lock()
-                    .await
-                    .write_all_plaintext(request.into().as_slice())
-                    .await
-                    .unwrap();
-            }
-        }
-    }
-
-    async fn write_tls(self: Arc<Self>) {
-        loop {
-            let mut tls_connection = self.tls_connection.lock().await;
-            let mut socket = self.socket.lock().await;
-            if tls_connection.wants_write() {
-                tls_connection.write_tls(&mut *socket).unwrap();
-            }
-        }
-    }
-
-    async fn read(self: Arc<Self>, mut response_sender: Sender<T>) {
-        loop {
-            let mut response = Vec::new();
-            match self
-                .tls_connection
-                .lock()
-                .await
-                .reader()
-                .read_to_end(&mut response)
-            {
-                Ok(_) => {}
-                Err(e) if e.kind() == ErrorKind::WouldBlock => {}
-                err => panic!("{:?}", err),
-            }
-            if !response.is_empty() {
-                response_sender.send(response.into()).await.unwrap();
-            }
-        }
-    }
-
-    async fn read_tls(self: Arc<Self>) {
-        loop {
-            let mut tls_connection = self.tls_connection.lock().await;
-            let mut socket = self.socket.lock().await;
-            if tls_connection.wants_read() {
-                match tls_connection.read_tls(&mut *socket) {
-                    Ok(_) => {}
-                    Err(e) if e.kind() == ErrorKind::WouldBlock => {}
-                    err => panic!("{:?}", err),
-                }
-                tls_connection.process_new_packets().await.unwrap();
-            }
-        }
+    pub fn run(&mut self, executor: &dyn Spawn) -> Result<(), ProverError> {
+        let run_future = self.run_future.take().ok_or(ProverError::AlreadyRunning)?;
+        executor.spawn(run_future).unwrap();
+        Ok(())
     }
 }
 
@@ -140,4 +95,6 @@ pub enum ProverError {
     IOError(#[from] std::io::Error),
     #[error(transparent)]
     InvalidSeverName(#[from] InvalidDnsNameError),
+    #[error("Prover is already running")]
+    AlreadyRunning,
 }
--- tls/prover/src/socket.rs
+++ tls/prover/src/socket.rs
@@ -1,22 +1,71 @@
-use futures::{
-    channel::mpsc::{Receiver, Sender},
-    AsyncRead,
+use bytes::Bytes;
+use futures::{sink::SinkMapErr, SinkExt};
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
 };
-use tokio_util::io::{SinkWriter, StreamReader};
+use tokio::{
+    io::{AsyncRead as TokioAsyncRead, AsyncWrite as TokioAsyncWrite, ReadBuf},
+    sync::mpsc::{Receiver, Sender},
+};
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_util::{
+    io::{CopyToBytes, SinkWriter, StreamReader},
+    sync::{PollSendError, PollSender},
+};
 
 pub struct AsyncSocket {
-    sink_writer: SinkWriter<Sender<Vec<u8>>>,
-    stream_reader: StreamReader<Receiver<Result<&'static [u8], std::io::Error>>, &'static [u8]>,
+    sink_writer: SinkWriter<
+        CopyToBytes<SinkMapErr<PollSender<Bytes>, fn(PollSendError<Bytes>) -> std::io::Error>>,
+    >,
+    stream_reader: StreamReader<ReceiverStream<Result<Bytes, std::io::Error>>, Bytes>,
 }
 
 impl AsyncSocket {
     pub fn new(
-        request_sender: Sender<Vec<u8>>,
-        response_receiver: Receiver<Result<&'static [u8], std::io::Error>>,
+        request_sender: Sender<Bytes>,
+        response_receiver: Receiver<Result<Bytes, std::io::Error>>,
     ) -> Self {
         Self {
-            sink_writer: SinkWriter::new(request_sender),
-            stream_reader: StreamReader::new(response_receiver),
+            sink_writer: SinkWriter::new(CopyToBytes::new(
+                PollSender::new(request_sender)
+                    .sink_map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err)),
+            )),
+            stream_reader: StreamReader::new(ReceiverStream::new(response_receiver)),
         }
+    }
+}
+
+impl TokioAsyncRead for AsyncSocket {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf,
+    ) -> Poll<Result<(), std::io::Error>> {
+        Pin::new(&mut self.stream_reader).poll_read(cx, buf)
+    }
+}
+
+impl TokioAsyncWrite for AsyncSocket {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        Pin::new(&mut self.sink_writer).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        Pin::new(&mut self.sink_writer).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        Pin::new(&mut self.sink_writer).poll_shutdown(cx)
     }
 }
