dffb7da58d283e3630995023c3d1a4245d56b633
th4s <th4s@metavoid.xyz>
2023-05-23 19:53:59 +0200

    Added test for close_notify

--- tls/prover/tests/prover.rs
+++ tls/prover/tests/prover.rs
@@ -6,10 +6,10 @@ use utils_aio::executor::SpawnCompatExt;
 
 #[tokio::test]
 async fn test_prover_run_parse_response_headers() {
-    let (mut prover, mut async_socket) = tlsn_new("tlsnotary.org");
+    let (mut prover, mut prover_handle) = tlsn_new("tlsnotary.org");
     prover.run(Handle::current().compat()).unwrap();
 
-    async_socket
+    prover_handle
             .write_all(
                     b"GET / HTTP/1.1\r\n\
                     Host: tlsnotary.org\r\n\
@@ -20,7 +20,7 @@ async fn test_prover_run_parse_response_headers() {
                 ).await.unwrap();
 
     _ = Handle::current().enter();
-    let (response_headers, _) = tokio::spawn(parse_response_headers(async_socket))
+    let (response_headers, _) = tokio::spawn(parse_response_headers(prover_handle))
         .await
         .unwrap();
     let parsed_headers = String::from_utf8(response_headers).unwrap();
@@ -30,11 +30,11 @@ async fn test_prover_run_parse_response_headers() {
 
 #[tokio::test]
 async fn test_prover_run_parse_response_body() {
-    let (mut prover, mut async_socket) = tlsn_new("tlsnotary.org");
+    let (mut prover, mut prover_handle) = tlsn_new("tlsnotary.org");
     prover.run(Handle::current().compat()).unwrap();
 
     // First we need to parse the response header again
-    async_socket
+    prover_handle
             .write_all(
                     b"GET / HTTP/1.1\r\n\
                     Host: tlsnotary.org\r\n\
@@ -45,7 +45,7 @@ async fn test_prover_run_parse_response_body() {
                 ).await.unwrap();
 
     _ = Handle::current().enter();
-    let (response_headers, async_socket) = tokio::spawn(parse_response_headers(async_socket))
+    let (response_headers, prover_handle) = tokio::spawn(parse_response_headers(prover_handle))
         .await
         .unwrap();
     let mut parsed_headers = String::from_utf8(response_headers).unwrap();
@@ -68,7 +68,7 @@ async fn test_prover_run_parse_response_body() {
     // get the remaining body length
     let body_start = parsed_headers.find("\r\n\r\n").unwrap() + 4;
     content_length -= parsed_headers.len() - body_start;
-    let response_body = tokio::spawn(parse_response_body(async_socket, content_length))
+    let response_body = tokio::spawn(parse_response_body(prover_handle, content_length))
         .await
         .unwrap();
 
@@ -81,11 +81,49 @@ async fn test_prover_run_parse_response_body() {
     assert!(parsed_body.contains("</html>"));
 }
 
+#[tokio::test]
+async fn test_prover_close_notify() {
+    let (mut prover, mut prover_handle) = tlsn_new("tlsnotary.org");
+    prover.run(Handle::current().compat()).unwrap();
+
+    prover_handle
+            .write_all(
+                    b"GET / HTTP/1.1\r\n\
+                    Host: tlsnotary.org\r\n\
+                    User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0\r\n\
+                    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n\
+                    Accept-Language: en-US,en;q=0.5\r\n\
+                    Accept-Encoding: identity\r\n\r\n"
+                ).await.unwrap();
+
+    _ = Handle::current().enter();
+    let (response_headers, mut prover_handle) = tokio::spawn(parse_response_headers(prover_handle))
+        .await
+        .unwrap();
+    let _parsed_headers = String::from_utf8(response_headers).unwrap();
+
+    prover_handle.close_tls().unwrap();
+    tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+
+    // This should fail, since we closed the tls connection
+    let expected_error = prover_handle
+            .write_all(
+                    b"GET / HTTP/1.1\r\n\
+                    Host: tlsnotary.org\r\n\
+                    User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0\r\n\
+                    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n\
+                    Accept-Language: en-US,en;q=0.5\r\n\
+                    Accept-Encoding: identity\r\n\r\n"
+                ).await;
+
+    assert!(matches!(expected_error, Err(std::io::Error { .. })));
+}
+
 fn tlsn_new(address: &str) -> (Prover, ProverHandle) {
     let tcp_stream = std::net::TcpStream::connect(&format!("{}:{}", address, "443")).unwrap();
     tcp_stream.set_nonblocking(true).unwrap();
 
-    let (prover, async_socket) = Prover::new(
+    let (prover, prover_handle) = Prover::new(
         ProverConfig::default(),
         address.to_owned(),
         Box::new(RustCryptoBackend::new()) as Box<dyn Backend>,
@@ -93,16 +131,16 @@ fn tlsn_new(address: &str) -> (Prover, ProverHandle) {
     )
     .unwrap();
 
-    (prover, async_socket)
+    (prover, prover_handle)
 }
 
-async fn parse_response_headers(mut async_socket: ProverHandle) -> (Vec<u8>, ProverHandle) {
+async fn parse_response_headers(mut prover_handle: ProverHandle) -> (Vec<u8>, ProverHandle) {
     let headers_end_marker = b"\r\n\r\n";
     let mut response_headers = vec![0; 1024];
     let mut read_bytes = 0;
 
     loop {
-        read_bytes += async_socket
+        read_bytes += prover_handle
             .read(&mut response_headers[read_bytes..])
             .await
             .unwrap();
@@ -120,11 +158,11 @@ async fn parse_response_headers(mut async_socket: ProverHandle) -> (Vec<u8>, Pro
     }
     response_headers.resize(read_bytes, 0);
 
-    (response_headers, async_socket)
+    (response_headers, prover_handle)
 }
 
-async fn parse_response_body(mut async_socket: ProverHandle, content_length: usize) -> Vec<u8> {
+async fn parse_response_body(mut prover_handle: ProverHandle, content_length: usize) -> Vec<u8> {
     let mut response_body: Vec<u8> = vec![0; content_length];
-    async_socket.read_exact(&mut response_body).await.unwrap();
+    prover_handle.read_exact(&mut response_body).await.unwrap();
     response_body
 }
