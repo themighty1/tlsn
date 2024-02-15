2d5d852f52b2059275c43137cb149ebd6f616f7e
dan <themighty1@users.noreply.github.com>
2024-02-15 11:20:49 +0200

    wip

--- authdecode/src/backend/halo2/mod.rs
+++ authdecode/src/backend/halo2/mod.rs
@@ -22,44 +22,44 @@ pub enum Curve {
     BN254,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{onetimesetup::OneTimeSetup, prover::Prover, verifier::Verifier, *};
-    use crate::tests::e2e_test;
+// #[cfg(test)]
+// mod tests {
+//     use super::{onetimesetup::OneTimeSetup, prover::Prover, verifier::Verifier, *};
+//     use crate::tests::e2e_test;
 
-    /// Run the whole authdecode protocol end-to-end, optionally corrupting the proof
-    /// if `will_corrupt_proof` is set to true.
-    fn halo2_e2e_test(will_corrupt_proof: bool) {
-        // The Prover should have generated the proving key (before the authdecode
-        // protocol starts) like this:
-        let proving_key = OneTimeSetup::proving_key();
+//     /// Run the whole authdecode protocol end-to-end, optionally corrupting the proof
+//     /// if `will_corrupt_proof` is set to true.
+//     fn halo2_e2e_test(will_corrupt_proof: bool) {
+//         // The Prover should have generated the proving key (before the authdecode
+//         // protocol starts) like this:
+//         let proving_key = OneTimeSetup::proving_key();
 
-        // The Verifier should have generated the verifying key (before the authdecode
-        // protocol starts) like this:
-        let verification_key = OneTimeSetup::verification_key();
+//         // The Verifier should have generated the verifying key (before the authdecode
+//         // protocol starts) like this:
+//         let verification_key = OneTimeSetup::verification_key();
 
-        let prover = Box::new(Prover::new(proving_key));
-        let verifier = Box::new(Verifier::new(verification_key, Curve::Pallas));
-        e2e_test(prover, verifier, will_corrupt_proof);
-    }
+//         let prover = Box::new(Prover::new(proving_key));
+//         let verifier = Box::new(Verifier::new(verification_key, Curve::Pallas));
+//         e2e_test(prover, verifier, will_corrupt_proof);
+//     }
 
-    #[test]
-    // As of Oct 2022 there appears to be a bug in halo2 which causes the prove
-    // times with MockProver be as long as with a real prover. Marking this test
-    // as expensive.
-    // #[ignore = "expensive"]
-    /// Tests that the protocol runs successfully
-    fn halo2_e2e_test_success() {
-        halo2_e2e_test(false)
-    }
+//     #[test]
+//     // As of Oct 2022 there appears to be a bug in halo2 which causes the prove
+//     // times with MockProver be as long as with a real prover. Marking this test
+//     // as expensive.
+//     // #[ignore = "expensive"]
+//     /// Tests that the protocol runs successfully
+//     fn halo2_e2e_test_success() {
+//         halo2_e2e_test(false)
+//     }
 
-    #[test]
-    // As of Oct 2022 there appears to be a bug in halo2 which causes the prove
-    // times with MockProver be as long as with a real prover. Marking this test
-    // as expensive.
-    // #[ignore = "expensive"]
-    /// Tests that a corrupted proof causes verification to fail
-    fn halo2_e2e_test_failure() {
-        halo2_e2e_test(true)
-    }
-}
+//     #[test]
+//     // As of Oct 2022 there appears to be a bug in halo2 which causes the prove
+//     // times with MockProver be as long as with a real prover. Marking this test
+//     // as expensive.
+//     // #[ignore = "expensive"]
+//     /// Tests that a corrupted proof causes verification to fail
+//     fn halo2_e2e_test_failure() {
+//         halo2_e2e_test(true)
+//     }
+// }
