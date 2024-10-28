5382be0208d74e0c2a2ab8ffd27f6c26db9e3f66
themighty1 <themighty1@users.noreply.github.com>
2024-10-28 09:19:30 +0000

    deploy: 25a0a0ba8aedaffbed8746886f36f9d18e494b80

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"tlsn_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.80.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"tlsn_core/enum.ServerName.html\" title=\"enum tlsn_core::ServerName\">ServerName</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["tlsn_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.82.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"tlsn_core/enum.ServerName.html\" title=\"enum tlsn_core::ServerName\">ServerName</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[379]}
